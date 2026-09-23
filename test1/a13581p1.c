06d1e48afa04268a01e50e32c3df9ad597a688a8
Senthalir viveka <senthalirviv@gmail.com>
2026-09-23 09:41:49 +0530

    Refactor div function for matrix handling
    Refactor matrix division function to use proper indexing and improve readability.

--- test1/a13581p1.c
+++ test1/a13581p1.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void div(int a[],int n)
+void div(int n,int a[n][n])
 {
     int A11[n/2][n/2],A12[n/2][n/2],A21[n/2][n/2],A22[n/2][n/2];
+
     printf("\n\nMatrix 1: ");
     for(int i=0;i<n/2;i++)
     {
-        for(int j=n/2;j<n;j++)
+        for(int j=0;j<n/2;j++)
         {
             A11[i][j]=a[i][j];
         }
-        //printf("\n");
     }
 
     printf("\n\nMatrix 2: ");
-    for(int i=n/2;i<n;i++)
+    for(int i=0;i<n/2;i++)
     {
         for(int j=0;j<n/2;j++)
         {
-            A12[i][j]=a[i][j];
+            A12[i][j]=a[i][j+n/2];
         }
-        //printf("\n");
     }
 
     printf("\n\nMatrix 3: ");
@@ -29,39 +28,37 @@ void div(int a[],int n)
     {
         for(int j=0;j<n/2;j++)
         {
-            A21[i][j]=a[i][j];
+            A21[i][j]=a[i+n/2][j];
         }
-        //printf("\n");
     }
 
     printf("\n\nMatrix 4: ");
-    for(int i=n/2;i<n;i++)
+    for(int i=0;i<n/2;i++)
     {
-        for(int j=n/2;j<n;j++)
+        for(int j=0;j<n/2;j++)
         {
-            A22[i][j]=a[i][j];
+            A22[i][j]=a[i+n/2][j+n/2];
         }
-        //printf("\n");
     }
 
     printf("\n\nMatrix 1: ");
     for(int i=0;i<n/2;i++)
     {
-        for(int j=n/2;j<n;j++)
+        for(int j=0;j<n/2;j++)
         {
             printf("%5d",A11[i][j]);
         }
-        //printf("\n");
+        printf("\n");
     }
 
     printf("\n\nMatrix 2: ");
-    for(int i=n/2;i<n;i++)
+    for(int i=0;i<n/2;i++)
     {
         for(int j=0;j<n/2;j++)
         {
             printf("%5d",A12[i][j]);
         }
-        //printf("\n");
+        printf("\n");
     }
 
     printf("\n\nMatrix 3: ");
@@ -71,20 +68,18 @@ void div(int a[],int n)
         {
             printf("%5d",A21[i][j]);
         }
-        //printf("\n");
+        printf("\n");
     }
 
     printf("\n\nMatrix 4: ");
-    for(int i=n/2;i<n;i++)
+    for(int i=0;i<n/2;i++)
     {
-        for(int j=n/2;j<n;j++)
+        for(int j=0;j<n/2;j++)
         {
             printf("%5d",A22[i][j]);
         }
-        //printf("\n");
+        printf("\n");
     }
-
-    return;
 }
 
 int main()
@@ -92,6 +87,7 @@ int main()
     int n;
     printf("Enter size of the matrix: ");
     scanf("%d",&n);
+
     int a[n][n];
 
     for(int i=0;i<n;i++)
@@ -102,15 +98,15 @@ int main()
         }
     }
 
+    printf("\nOriginal Matrix:\n");
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
         {
-            printf("%d",a[i][j]);
+            printf("%5d",a[i][j]);
         }
+        printf("\n");
     }
 
-    div(a,n);
-
-
+    div(n,a);
 }
